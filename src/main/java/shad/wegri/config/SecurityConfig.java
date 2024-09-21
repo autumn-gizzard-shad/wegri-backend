deb44df4299becc4621b127170e5fa11537cd276
Lucerna00 <pjs3859811@naver.com>
2024-09-21 15:58:20 +0900

    Feat: JWT를 위한 config 추가

---
+++ src/main/java/shad/wegri/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package shad.wegri.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import shad.wegri.jwt.JwtAuthenticationFilter;
+import shad.wegri.jwt.JwtProvider;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .formLogin((config) -> config.disable())
+            .httpBasic((config) -> config.disable())
+            .csrf((config) -> config.disable())
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeRequests((request) -> request
+                // 특정 API 요청만 허용
+                .requestMatchers("/api/members/register", "/api/members/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            )
+            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+}
