309cde2098bb8316aaecc76cb5cdd764e4ca51ce
Lucerna00 <pjs3859811@naver.com>
2024-09-21 20:40:27 +0900

    Feat: CORS 설정

---
+++ src/main/java/shad/wegri/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package shad.wegri.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type");
+    }
+}
